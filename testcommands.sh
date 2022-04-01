0917fca6d8b153122cdd026ccc47e4c7544d2060
pathange-s <sainatharao@gmail.com>
2022-04-01 15:32:54 +0530

    test ci cd 2

--- testcommands.sh
+++ testcommands.sh
@@ -2,3 +2,22 @@
 set -e
 
 npm list -g | grep neu
+
+echo "uninstalling neu"
+npm uninstall -g @neutralinojs/neu
+echo "installing neu 8.1.0"
+npm install -g @neutralinojs/neu@8.1.0
+echo "check neu version"
+neu version
+echo "Add plugin"
+neu plugins --add @neutralinojs/appify
+
+echo "check if its added in which node_modules"
+pwd
+
+
+echo "updating neu"
+npm install -g @neutralinojs/neu
+
+echo "list plugins"
+neu plugins
