79184fdaf146d1e8be01329a229bb057249f939d
pathange-s <sainatharao@gmail.com>
2022-04-03 21:38:41 +0530

    test check 9

--- scripts/testcommands.sh
+++ scripts/testcommands.sh
@@ -1,137 +1,133 @@
 #!/bin/bash
 set -e
 
-ldconfig --version
+mkdir testNeuCLI
+cd testNeuCLI
 
-ldconfig -p | grep libwebkit2gtk-4.0.so.37
+# NEU
 
-# mkdir testNeuCLI
-# cd testNeuCLI
+displayCmd(){
+    echo -e "\e[1;32mcmd: $1\e[0m"
+}
 
-# # NEU
+echoGreen(){
+    echo -e "\e[1;32m$1"
+}
 
-# displayCmd(){
-#     echo -e "\e[1;32mcmd: $1\e[0m"
-# }
+echo -e "\e[1;32mTesting Neutralinojs CLI commands\n"
 
-# echoGreen(){
-#     echo -e "\e[1;32m$1"
-# }
+displayCmd "neu --help"
+neu --help
+echo
 
-# echo -e "\e[1;32mTesting Neutralinojs CLI commands\n"
+# create
 
-# displayCmd "neu --help"
-# neu --help
-# echo
+displayCmd "neu create <app name>"
+neu create myapp
+echo
 
-# # create
+displayCmd "neu create <app name> --template <template>"
+neu create myapp-zero --template neutralinojs/neutralinojs-zero
+echo
 
-# displayCmd "neu create <app name>"
-# neu create myapp
-# echo
+displayCmd "neu create --help"
+neu create --help
+echo
 
-# displayCmd "neu create <app name> --template <template>"
-# neu create myapp-zero --template neutralinojs/neutralinojs-zero
-# echo
+# version (global version)
 
-# displayCmd "neu create --help"
-# neu create --help
-# echo
+displayCmd "neu version"
+neu version
+echo
 
-# # version (global version)
+# run
 
-# displayCmd "neu version"
-# neu version
-# echo
+sudo apt-get install xvfb
 
-# # run
+echoGreen "Creating a sample app before running neu run & neu build"
+neu create myapp-run
+cd myapp-run
+echo
 
-# sudo apt-get install xvfb
+displayCmd "neu run"
+neu run
+echo
 
-# echoGreen "Creating a sample app before running neu run & neu build"
-# neu create myapp-run
-# cd myapp-run
-# echo
+displayCmd "neu run --disable-auto-reload"
+neu run --disable-auto-reload
+echo
 
-# displayCmd "neu run"
-# neu run
-# echo
+displayCmd "neu run --arch"
+neu run --arch x64
+echo
 
-# displayCmd "neu run --disable-auto-reload"
-# neu run --disable-auto-reload
-# echo
+echoGreen "Creating the environment to run the app with flag --frontend-lib-dev"
+echo
+neu create myapp-react --template codezri/neutralinojs-react
+echo
+cd myapp-react && cd react-src && npm i && npm run build
+npm start &
+displayCmd "neu run --frontend-lib-dev"
+until [ ! -z "$(sudo netstat -tulpn | grep :3000)" ];
+do
+  echo "starting development server"
+  sleep 3s
+done
+cd .. && neu run --frontend-lib-dev
+sudo kill `lsof -t -i:3000`
+echo
 
-# displayCmd "neu run --arch"
-# neu run --arch x64
-# echo
+displayCmd "neu run --help"
+neu run --help
+echo
 
-# echoGreen "Creating the environment to run the app with flag --frontend-lib-dev"
-# echo
-# neu create myapp-react --template codezri/neutralinojs-react
-# echo
-# cd myapp-react && cd react-src && npm i && npm run build
-# npm start &
-# displayCmd "neu run --frontend-lib-dev"
-# until [ ! -z "$(sudo netstat -tulpn | grep :3000)" ];
-# do
-#   echo "starting development server"
-#   sleep 3s
-# done
-# cd .. && neu run --frontend-lib-dev
-# sudo kill `lsof -t -i:3000`
-# echo
+# build
 
-# displayCmd "neu run --help"
-# neu run --help
-# echo
+displayCmd "neu build"
+neu build
+echo
 
-# # build
+displayCmd "neu build --release"
+neu build --release
+echo
 
-# displayCmd "neu build"
-# neu build
-# echo
+echoGreen "Creating .storage directory before running neu build --copy-storage"
+mkdir .storage
+echo
 
-# displayCmd "neu build --release"
-# neu build --release
-# echo
+displayCmd "neu build --copy-storage"
+neu build --copy-storage
+echo
 
-# echoGreen "Creating .storage directory before running neu build --copy-storage"
-# mkdir .storage
-# echo
+displayCmd "neu build --help"
+neu build --help
+echo
 
-# displayCmd "neu build --copy-storage"
-# neu build --copy-storage
-# echo
+# update
 
-# displayCmd "neu build --help"
-# neu build --help
-# echo
+displayCmd "neu update"
+neu update
+echo
 
-# # update
+# version (project-specific versions)
 
-# displayCmd "neu update"
-# neu update
-# echo
+displayCmd "neu version"
+neu version
+echo
 
-# # version (project-specific versions)
+# plugins
 
-# displayCmd "neu version"
-# neu version
-# echo
+displayCmd "neu plugins --add <plugin>"
+neu plugins --add @neutralinojs/appify
+echo
 
-# # plugins
+displayCmd "neu plugins --remove <plugin>"
+neu plugins --remove @neutralinojs/appify
+echo
 
-# displayCmd "neu plugins --add <plugin>"
-# neu plugins --add @neutralinojs/appify
-# echo
+displayCmd "neu plugins --help"
+neu plugins --help
+echo
 
-# displayCmd "neu plugins --remove <plugin>"
-# neu plugins --remove @neutralinojs/appify
-# echo
-
-# displayCmd "neu plugins --help"
-# neu plugins --help
-# echo
-
-# pwd
-# sudo rm -r ../../../testNeuCLI
+pwd
+sudo rm -r ../../../testNeuCLI
