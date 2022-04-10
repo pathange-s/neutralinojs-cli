fd68b52d421b56c7ce6be474e8e5d6c6fa91a394
pathange-s <sainatharao@gmail.com>
2022-04-10 11:40:34 +0530

    Plugins wont disappear after updating neu cli

--- scripts/test_commands.sh
+++ scripts/test_commands.sh
@@ -141,3 +141,32 @@ echo
 displayCmd "neu plugins --help"
 neu plugins --help
 echo
+
+# Check if plugins remain after updating CLI
+
+echoGreen "uninstalling current version of neu"
+npm uninstall -g @neutralinojs/neu
+echoGreen "installing neu 8.1.0 (older version)"
+npm install -g @neutralinojs/neu@8.1.0
+echoGreen "check neu version"
+neu version
+
+echoGreen "Add plugin"
+neu plugins --add @neutralinojs/appify
+ 
+echoGreen "updating neu"
+npm install -g @neutralinojs/neu
+ 
+npm link
+
+# Check if list of plugins has previously installed plugins 
+# Attempt to install plugin occurs
+
+echoGreen "list plugins"
+neu plugins
+
+echoGreen "list plugins after reinstalling it"
+neu plugins
+
+echoGreen "Removing plugin to revert to initial state"
+neu plugins --remove @neutralinojs/appify
